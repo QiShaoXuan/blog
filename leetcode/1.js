5e01f4d28ba242af5187ab9e5c5dfa6fd1da2677
qishaoxuan <qishaoxuan@jd.com>
2022-01-11 22:18:55 +0800

    feat: update leetcode 1

--- leetcode/1.js
+++ leetcode/1.js
@@ -1,8 +1,30 @@
-var twoSum = function (nums, target) {
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// 暴力
+const twoSum1 = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    let arrIndex = nums.findIndex(v => v === target - nums[i])
-    if (arrIndex !== -1 && arrIndex !== i) {
-      return [i, arrIndex]
+    const n = nums[i]
+    const lack = target - n
+
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] === lack) {
+        return [i, j]
+      }
     }
+  }
+}
+
+//  哈希
+const twoSum2 = function (nums, target) {
+  const map = {}
+  for (let i = 0; i < nums.length; i++) {
+    const lack = target - nums[i]
+    if (map[lack] !== undefined) {
+      return [map[lack], i]
+    }
+    map[nums[i]] = i
   }
 }
